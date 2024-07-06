84ec0734a524aeeb0c59c99073a6e91bb47b987a
psurendiran <psurendiran@osius.com>
2024-07-07 01:34:46 +0530

    "setup few pages"

--- app/categories/women/page.tsx
+++ app/categories/women/page.tsx
@@ -1,7 +1,25 @@
-export default function Page() {
+import Header from "@/app/components/Header";
+import {auth} from "@/app/lib/auth";
+import prisma from "@/app/lib/db";
+
+export default async function Page() {
+  const session = await auth();
+  const user = await prisma.user.findUnique({
+    where: {email: session?.user?.email!},
+    select: {name: true, email: true, image: true},
+  });
+
   return (
-    <div>
-      <h1>Women</h1>
+    <div className='flex flex-col'>
+      <Header user={user} />
+      <main className='flex-1 px-14 bg-muted'>
+        <section className='w-full py-10 md:pt-32'>
+          <div>
+            <h1>Womens</h1>
+            <p>Discover the latest Womens collection.</p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
