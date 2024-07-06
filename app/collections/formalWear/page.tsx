4669ead85c5c2f71cac460982f261fe58a4d4612
psurendiran <psurendiran@osius.com>
2024-07-07 00:33:14 +0530

    "fixed a error in formalWear page"

--- app/collections/formalWear/page.tsx
+++ app/collections/formalWear/page.tsx
@@ -1,12 +1,17 @@
 import Header from "@/app/components/Header";
 import {auth} from "@/app/lib/auth";
+import prisma from "@/app/lib/db";
 
 export default async function Page() {
   const session = await auth();
+  const user = await prisma.user.findUnique({
+    where: {email: session?.user?.email!},
+    select: {name: true, email: true, image: true},
+  });
 
   return (
     <div className='flex flex-col'>
-      <Header session={session} />
+      <Header user={user} />
       <main className='flex-1 px-14 bg-muted'>
         <section className='w-full py-10 md:pt-32'>
           <div>
